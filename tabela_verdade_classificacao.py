16da3b70035e9c807433f4dd6b8aff004ed093e0
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-08-28 08:38:24 -0300

    Update tabela_verdade_classificacao.py

--- tabela_verdade_classificacao.py
+++ tabela_verdade_classificacao.py
@@ -10,11 +10,12 @@ while True:
     print("[1] Use qualquer letra para indicar uma proposição, como por exemplo, p, q, r,...\n"
           "[2] Os operadores lógicos são indicados por:\n"
           "'não' -> '~', 'e' -> 'and', 'ou' -> 'or', 'implicação' -> '=>', 'dupla implicação' -> '='\n"
-          "Por exemplo, para o cálculo do valor lógico das expressões\n"
-          "                                     'p and q' e 'p or q'                                     \n"
+          "Por exemplo, para o cálculo do valor lógico da proposição\n"
+          "                                     'p and q'                                     \n"
           "digite:\n"
           "['p', 'q'] na primeira entrada (proposições simples) e\n"
-          "['p and q', 'p or q'] na segunda entrada (proposições compostas)")
+          "['p and q'] na segunda entrada (proposições compostas)\n"
+          "Em particular, o exemplo em questão trata-se de uma contingência.")
     print('****************************************SAÍDA DE DADOS*****************************************')
     a = input('Digite as proposições simples: ')
     b = input('Digite as propsições compostas: ')
